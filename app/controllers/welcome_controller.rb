b24f53cdace4f6793077cd78ad4ac0f2c86ac956
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2015-01-05 20:54:48 +0100

    Add posts to homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-
+    @posts = [
+      Post.new(:title => "Post 1", :subtitle => "Lorem ispsum...", :content => "Some stupid content..."),
+      Post.new(:title => "Post 2", :subtitle => "Lorem ispsum...", :content => "Some stupid content...")
+    ]
   end
 end
